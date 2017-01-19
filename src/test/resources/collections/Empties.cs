79b416668c654ed2abb9df8c31f21ee47b9805a8
stasptr <stasptr@gmail.com>
2017-01-19 22:49:50 +0200

    fixed tests

---
+++ src/test/resources/collections/Empties.cs
@@ -0,0 +1,19 @@
+namespace collections
+{
+	internal class Empties
+	{
+		public virtual void test()
+		{
+			System.Collections.Generic.IList<string> list = new System.Collections.Generic.List<
+				string>();
+			System.Collections.Generic.ICollection<string> collection = new System.Collections.Generic.List<
+				string>();
+			System.Collections.Generic.ICollection<string> set = new System.Collections.Generic.HashSet<
+				string>();
+			System.Collections.Generic.ICollection<string> collection2 = new System.Collections.Generic.HashSet<
+				string>();
+			System.Collections.Generic.IDictionary<int, string> map = new System.Collections.Generic.Dictionary<
+				int, string>();
+		}
+	}
+}
